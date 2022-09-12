779128a08fc11dab77e841fe6c0529105b0c5f73
VidaBirago <biragoadasi@gmail.com>
2022-09-12 19:38:24 +0000

    c programming

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ * main - main block
+ * Return: l
+ */
+int main(void)
+{
+	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",s);
+	return (1);
+}
