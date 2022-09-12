b1ee705bf1168a8fcf8b737e773cc6d0fe871cc6
VidaBirago <biragoadasi@gmail.com>
2022-09-12 20:22:26 +0000

    c progrfamming# ;

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 /**
- * main - main block
- * Return: l
+ * main - Prints out the last part of a qoute in the standard error.
+ * Return: l if success.
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",s);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
